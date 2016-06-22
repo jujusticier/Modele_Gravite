b97b63a8a8085496e152376e793adf811fac5f75
kl4k <kl4k.0s@gmail.com>
2016-06-22 17:45:45 +0200

    Update Script Requete banque mondiale.R

--- Script Requete banque mondiale.R
+++ Script Requete banque mondiale.R
@@ -69,5 +69,5 @@ distQueryCheck()
 ######################### avec Here
 
 https://route.cit.api.here.com/routing/7.2/calculateroute.json?waypoint0=52.5129%2C13.4037&waypoint1=52.5206%2C13.3862&mode=fastest%3Bcar%3Btraffic%3Aenabled&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&departure=now
-
+https://route.cit.api.here.com/routing/7.2/calculateroute.json?waypoint0=48.87%2C2.29&waypoint1=40.71%2C-74.01&mode=fastest%3Bcar%3Btraffic%3Aenabled&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&
 #ensuite il faudra merger les villes par pays (fois 2) puis agregate par couple ij
