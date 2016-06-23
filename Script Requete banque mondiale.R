294081234cec7aea002c98c4baceaa9770e22247
kl4k <kl4k.0s@gmail.com>
2016-06-23 10:11:58 +0200

    Update Script Requete banque mondiale.R

--- Script Requete banque mondiale.R
+++ Script Requete banque mondiale.R
@@ -1,6 +1,6 @@
 
 
-#Script pour les donn??es API Banque mondiale 
+#Script pour les données API Banque mondiale 
 library(jsonlite)
 conversion2 <- read.csv2(file=file.choose())
 nbpage <- c(requeteAPI[[1]]$pages)
@@ -33,44 +33,3 @@ write.csv(x=Datacomplete, file="F:/POP.csv")
 library(eurostat)
 library(dplyr)
 test <- get_eurostat("fats_out2_r2", Filters=list(geo="FR"))
-
-
-##########################################
-#Script donnees geo
-
-library(data.table)
-library(ggmap)
-library(dplyr)
-cities<- read.csv2(file.choose())
-#pour g??n??rer les couples de villes
-x <- list()
-for(i in cities$City){
-  x[[i]]<- data.frame(rep(i, times= 229),cities$City)
-}
-#table g??n??r??e
-TableVille <- rbindlist(x)
-TableVille$rep.i..times...229. <- as.character(TableVille$rep.i..times...229.)
-TableVille$cities.City <- as.character(TableVille$cities.City )
-
-
-#pour la distance entre les villes il va surement falloir utiliser OSRM
-x <-list()
-for(i in 555:556){
-from<- c(TableVille$rep.i..times...229.[i:i])
-to <- c(TableVille$cities.City[i:i])
-x[[i]]<-mapdist(from,to,output = c("simple"),mode = c("driving"))
-message("Traite la ligne",i)
-}
-
-tabledistance <- rbindlist(x)
-distQueryCheck()
-
-
-######################### avec Here
-
-https://route.cit.api.here.com/routing/7.2/calculateroute.json?waypoint0=52.5129%2C13.4037&waypoint1=52.5206%2C13.3862&mode=fastest%3Bcar%3Btraffic%3Aenabled&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&departure=now
-https://route.cit.api.here.com/routing/7.2/calculateroute.json?waypoint0=48.87%2C2.29&waypoint1=40.71%2C-74.01&mode=fastest%3Bcar%3Btraffic%3Aenabled&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&
-#ensuite il faudra merger les villes par pays (fois 2) puis agregate par couple ij
-
-
-http://router.project-osrm.org/viaroute?loc=52.503033,13.420526&loc=52.516582,13.429290&instructions=true
