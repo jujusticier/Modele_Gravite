a40f0c43b285f8dfa99995732a412f5e127e0769
kl4k <kl4k.0s@gmail.com>
2016-07-05 16:59:57 +0200

    Update Bing.R

--- Scripts_Distance/Bing.R
+++ Scripts_Distance/Bing.R
@@ -1,4 +1,5 @@
 #### script avec BING
 
 http://dev.virtualearth.net/REST/V1/Routes/Driving?wp.0=redmond%2Cwa&wp.1=Issaquah%2Cwa&avoid=minimizeTolls&key=BingMapsKey
+AhGCyLgs4kFp3JlLL2koinDWMHdQI1xNRLTVDGatqoxBFkqN8CuJ0S_5UeYo7Ur8
 
